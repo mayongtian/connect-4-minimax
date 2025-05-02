132db2130c32b599edab7ab40ab43eda97b16a6f
Yongtian Ma <jema9047@uni.sydney.edu.au>
2025-05-03 01:07:15 +1000

    gets means and stdev
    next is actually applying bayes

--- A2/naive_bayes.py
+++ A2/naive_bayes.py
@@ -3,9 +3,9 @@
 import sys
 import os
 import csv
+import math
 
 def classify_nb(training_filename, testing_filename):
-    print(training_filename)
 
     # file checks
     if not os.path.exists(training_filename):
@@ -16,126 +16,40 @@ def classify_nb(training_filename, testing_filename):
         return []
     
     #training
+
+    # gets means
+    n_columns = 0
+    means = []
+    n_rows = 1
     with open(training_filename, newline="") as training_file:
         reader_training = csv.reader(training_file, delimiter=',', quotechar='|')
-
-        """
-        training_data = []
-        for row in reader_training:
-            if row != []:
-                new_row = []
-                for i in row:
-                    if isfloat(i):
-                        new_row.append(float(i))
-                    else:
-                        new_row.append(i)
-                training_data.append(new_row)
-        """
         # the file I submitted has a blank line at the beginning :(
-        next_line = reader_training.__next__()
-        while next_line == "":
-            next_line = reader_training.__next__()
+        first_line = reader_training.__next__()
+        while first_line == []:
+            first_line = reader_training.__next__()
 
-    # checks if it is discrete or normalised - normalised data will haeve the first entry start with a "0"
-    n_columns = len(next_line) - 1
-    if next_line == "0":
-        normalized = True
-    else:
-        normalized = False
-        return classifier_discrete(training_filename, testing_filename, n_columns)
-    
-    print(normalized)
+        n_columns = len(first_line) - 1
+        for i in range(n_columns):
+            means.append(float(first_line[i]))
         
+        for row in reader_training:
+            n_rows += 1
+            for i in range(n_columns):
+                means[i] = ( float(means[i]) * (n_rows - 1) + float(row[i]))/n_rows
 
-def classifier_discrete(training_filename, testing_filename, n_columns):
-        n_E_array, n_EandH_array, n_total, n_yes = training_discrete(training_filename, n_columns)
-
-        return testing_discrete(testing_filename, n_columns, n_total, n_E_array, n_EandH_array, n_yes)
-
-def training_discrete(training_filename, n_columns):
-    # converts the csv into an array of n(E) where n gives the number of appearances the evidence has
-
-    n_E_array = [
-        [0] * n_columns,
-        [0] * n_columns,
-        [0] * n_columns,
-        [0] * n_columns
-    ]
-    # n_EandH_array[0] represents low, 2nd row represents med, then high, then very high. The i, jth entry represents the number of times that the variable j is equal to i
-
-    n_EandH_array = [
-        [0] * n_columns,
-        [0] * n_columns,
-        [0] * n_columns,
-        [0] * n_columns
-    ]
-    # similar but also must be yes
-    n_total = 0
-    n_yes = 0
-
+    # gets stdev
+    stdev = [0] * n_columns
     with open(training_filename, newline="") as training_file:
         reader_training = csv.reader(training_file, delimiter=',', quotechar='|')
+        sigma = [0] * n_columns # sum of square of differences
         for row in reader_training:
-            print(row)
-            if row == []:
-                continue
-            n_total += 1
-            for i in range(n_columns):
-                if row[i] == "low":
-                    n_E_array[0][i] += 1
-                elif row[i] == "medium":
-                    n_E_array[1][i] += 1
-                elif row[i] == "high":
-                    n_E_array[2][i] += 1
-                elif row[i] == "very high":
-                    n_E_array[3][i] += 1
-                else:
-                    # print(f"unknown discrete variable {row[i]}", file=sys.stderr)
-                    pass
-
-            if row[n_columns] == "yes":
-                n_yes += 1
-                for i in range(n_columns):
-                    if row[i] == "low":
-                        n_EandH_array[0][i] += 1
-                    elif row[i] == "medium":
-                        n_EandH_array[1][i] += 1
-                    elif row[i] == "high":
-                        n_EandH_array[2][i] += 1
-                    elif row[i] == "very high":
-                        n_EandH_array[3][i] += 1
-    return n_E_array, n_EandH_array, n_total, n_yes
-
-def testing_discrete(testing_filename, n_columns, n_total, n_E_array, n_EandH_array, n_yes):
-    # list of probabilities P(yes|Etotal)
-    probabilities = []
-    with open(testing_filename, newline="") as f:
-        reader = csv.reader(f, delimiter=',', quotechar='|')
-        for row in reader:
-            row_prob = []
-            
             if row == []:
                 continue
             for i in range(n_columns):
-                prob = 0 # p(Ei|yes)
-                if row[i] == "low":
-                    prob = bayes(n_yes, n_EandH_array[0][i], n_E_array[0][i], n_total)
-                elif row[i] == "medium":
-                    prob = bayes(n_yes, n_EandH_array[1][i], n_E_array[0][i], n_total)
-                elif row[i] == "high":
-                    prob = bayes(n_yes, n_EandH_array[2][i], n_E_array[0][i], n_total)
-                elif row[i] == "very high":
-                    prob = bayes(n_yes, n_EandH_array[3][i], n_E_array[0][i], n_total)
-                row_prob.append(prob)
-            probabilities.append(bayes_flexible(n_yes, n_total, row_prob))
-    ret = []
-    for i in probabilities:
-        if i >= 0.5:
-            ret.append("yes")
-        else:
-            ret.append("no")
-        
-    return ret
+                val = float(row[i])
+                sigma[i] += (val - means[i])**2
+
+        for i in range(n_columns): stdev[i] = math.sqrt(sigma[i]/(n_rows - 1))
 
 
 def isfloat(num):
@@ -145,18 +59,5 @@ def isfloat(num):
     except ValueError:
         return False
     
-def bayes(n_E, n_EandH, n_H, n_total):
-    """returns P(H|E)"""
-    return (float(n_EandH)/n_H * float(n_H)/n_total)/(float(n_E)/n_total)
 
-# returns p(H|E) for multiple variables
-def bayes_flexible(n_yes : int, n_E : int, Ei : list[int]):
-    """
-    :param Ei: list of bayes probability P(Ei|H) for each variable
-    """
-    ret = float(n_yes)/n_E
-    for i in Ei:
-        ret *= i
-    return ret
-
-print(classify_nb("A2/pima-indians-diabetes-discrete.csv", "A2/pima-indians-diabetes-test-discrete.csv"))
+print(classify_nb("A2/pima-indians-diabetes-normalized.csv", "A2/pima-indians-diabetes-test-discrete.csv"))
